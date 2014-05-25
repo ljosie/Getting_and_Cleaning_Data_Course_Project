c077bfa827e22bc636d1cdc135b5117ad386c6b2
ljosie <ljosie.tao@gmail.com>
2014-05-25 22:08:09 +0800

    first commit

---
+++ run_analysis.R
@@ -0,0 +1,9 @@
+## Getting and Cleaning Data on Coursera -- Course Project
+## Author: Josie.Tao Date: 24/05/2014 
+## run_analysis.R that does the following:
+## 1. Merges the training and the test sets to create one data set.
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+## 3. Uses descriptive activity names to name the activities in the data set Appropriately
+##    labels the data set with descriptive activity names. 
+## 4. Creates a second, independent tidy data set with the average of each variable for each 
+##    activity and each subject. 
